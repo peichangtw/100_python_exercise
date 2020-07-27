3079e50a46633950a078d0b769d2b778d2ee7513
Pei Chang <peibest@gmail.com>
2020-07-27 11:19:32 -0700

    execrise 1

---
+++ 01.py
@@ -0,0 +1,18 @@
+'''Write a program which will find all such numbers 
+    which are divisible by 7 but are not a multiple of 5, 
+    between 2000 and 3200 (both included).
+    The numbers obtained should be printed in a comma-separated 
+    sequence on a single line.
+'''
+
+result = []
+for i in range(2000, 3200):
+    if i % 7 == 0 and i % 5 != 0:
+        print(i, end=', ')
+        result.append(i)
+        #print(result)
+
+#print the number in list
+print([i for i in range(2000,3201) if i%7 == 0 and i%5 != 0 ])
+#print the number in sring 
+print(*(i for i in range(2000,3201) if i%7 == 0 and i%5 != 0 ), sep=',')
